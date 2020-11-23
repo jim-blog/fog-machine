13bddfef34d547e344513d7a036f84e65b817283
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-11-23 17:02:21 +0100

    Add PropButton component in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class PropButton extends Component {
+  render() {
+    const {
+      onClick,
+      className="",
+        children,
+    } = this.props;
+
+    return (
+        <button
+          onClick={onClick}
+          className={className}
+          type="button"
+          >
+          {children}
+        </button>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <PropButton onClick={() => alert("clicked!")}>
+          POWER ON
+        </PropButton>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
