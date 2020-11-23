08c1bf51c6d457d77459c732d1beaeb0cf04ea5b
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-11-23 17:44:51 +0100

    PropButton with Material Ui Button

--- src/App.js
+++ src/App.js
@@ -1,49 +1,43 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button'
 import './App.css';
 
 class PropButton extends Component {
-  render() {
-    const {
-      onClick,
-      className="",
-        children,
-    } = this.props;
+    render() {
+        const {
+            onClick,
+            className = "",
+            children,
+        } = this.props;
 
-    return (
-        <button
-          onClick={onClick}
-          className={className}
-          type="button"
-          >
-          {children}
-        </button>
-    );
-  }
+        return (
+            <Button
+                onClick={onClick}
+                className={className}
+                variant="contained"
+                color="primary"
+            >
+                {children}
+            </Button>
+        );
+    }
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <PropButton onClick={() => alert("clicked!")}>
-          POWER ON
-        </PropButton>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Fog Machine Control
+                </p>
+            </header>
+            <div className="App-panel">
+                <PropButton onClick={() => alert("clicked!")}>
+                    POWER ON
+                </PropButton>
+            </div>
+        </div>
+    );
 }
 
 export default App;
