3aa9ce6f17926820357361e73d5a6d36fe9d8a89
fauresystems <34378695+fauresystems@users.noreply.github.com>
2020-12-03 18:50:21 +0100

    Update ArduinoButton.js

--- src/components/ArduinoButton.js
+++ src/components/ArduinoButton.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react";
-import Button from "@material-ui/core/Button";
+import React, {Component} from 'react'
+import Button from '@material-ui/core/Button';
 
 class ArduinoButton extends Component {
     render() {
